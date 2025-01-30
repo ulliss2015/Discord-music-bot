db03681c8caa622f99e6715960483d19d610cafd
ulliss2015 <kudo2515@gmail.com>
2025-01-30 22:00:24 +0200

    cookie add

--- bot_main.py
+++ bot_main.py
@@ -156,6 +156,7 @@ async def play(ctx, *, query):
         'no_warnings': True,
         'default_search': 'auto',
         'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'cookiefile': 'cookies.txt',
     }
 
     try:
